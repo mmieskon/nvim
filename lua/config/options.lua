57d05767b1625544f5f6aadb56abdc9259780899
mmieskon <105143729+mmieskon@users.noreply.github.com>
2023-07-12 12:18:53 +0300

    Make search case insensitive

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 
 vim.opt.clipboard = "unnamedplus"
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
@@ -10,5 +13,4 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.opt.scrolloff = 8
-
 
